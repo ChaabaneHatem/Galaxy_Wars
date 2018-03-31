08bad9f59accc2f1f3aba229d3827d4a7b79c9b3
ChaabaneHatem <hatemchaabane0@gmail.com>
2018-03-31 11:58:02 -0400

    adjust tag and layer, ignore inefficient collision layer

--- Assets/Scripts/Services/GV.cs
+++ Assets/Scripts/Services/GV.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GV : MonoBehaviour {
+public class GV
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //planet informations
+    public enum PlANET_TYPE { ENEMY, PLAYER };
+    public enum PLANET_LEVEL { LEVEL_I, LEVEL_II, LEVEL_III }
+    public static readonly float PLANET_MAX_PARTICULE_PER_LEVEL = 25;
+    public static readonly string PLAYER_PLANET_TAG = "PlayerPlanet";
+    public static readonly string ENEMY_PLANET_TAG = "EnemyPlanet";
+
+
+    //particule informations
+    public enum PARTICULE_TYPE { ENEMY, PLAYER };
+    public static readonly float PARTICULE_VALUE = 1;
+    public static readonly string PLAYER_PARTICULE_TAG = "ParticulePlayer";
+    public static readonly string ENEMY_PARTICULE_TAG = "ParticuleEnemy";
+
+
+    //useful function
+    public static float GetRandomFloat(Vector2 vector)
+    {
+        return Random.Range(vector.x, vector.y);
+    }
+
+    public static int GetRandomInt(Vector2 vector)
+    {
+        return (int)Random.Range(vector.x, vector.y);
+    }
+
 }
